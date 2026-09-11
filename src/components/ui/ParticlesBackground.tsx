6d227d542a503db1e94d0a0cd28ad304089b15b1
Sunfi <sunfisazzad@gmail.com>
2026-09-11 06:37:41 +0600

    feat: set desktop particle count to 100

--- src/components/ui/ParticlesBackground.tsx
+++ src/components/ui/ParticlesBackground.tsx
@@ -95,7 +95,7 @@ export function ParticlesBackground() {
             },
             number: {
               density: { enable: false },
-              value: isMobile ? 35 : 90,
+              value: isMobile ? 35 : 100,
             },
             opacity: {
               value: { min: 0.4, max: 0.8 },
